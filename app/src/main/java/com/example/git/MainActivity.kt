334675403537b146aefc158fb22804f9d2d7731b
desenvolvimento-wepickup <desenvolvimento@wepickup.pt>
2021-03-07 17:47:25 +0000

    segundo commit

--- app/src/main/java/com/example/git/MainActivity.kt
+++ app/src/main/java/com/example/git/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //primeira alteração (primeiro commit)
+
+        //segunda alteração (segundo commit)
     }
 }
